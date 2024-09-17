5ae0817007882b669c2c856abff73c8e17facc2f
maliubiao <maliubiao@gmail.com>
2024-09-17 14:54:29 +0800

    test ok

--- test_api.sh
+++ test_api.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Test the /get_html endpoint
-curl -X GET "http://localhost:8888/get_html?url=https://example.com"
+curl -X GET "http://localhost:8888/get_html?url=https://baidu.com"
+
+
--- tornado_server.py
+++ tornado_server.py
@@ -56,7 +56,7 @@ class GetHtmlHandler(tornado.web.RequestHandler):
 
         try:
             html = yield tornado.gen.with_timeout(datetime.timedelta(milliseconds=5000), future)
-            self.write(f"<!DOCTYPE html>{html}")
+            self.write(html)
         except tornado.gen.TimeoutError:
             self.set_status(504)
             self.write("Request timed out")
